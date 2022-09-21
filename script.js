04ab39d0cb75c5e1107b13a82498d7a99801b469
jonpfluger <jonpfluger718@gmail.com>
2022-09-21 11:23:55 -0500

    refactored the questions into objects, added functions to handle answer and display question

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var highscore = 0;
-var secondsLeft = 60;
 var body = document.body;
 var myH1 = document.getElementById("myh1");
 var mySection = document.getElementById("mysection")
@@ -11,27 +9,128 @@ var footerH4 = document.getElementById("footerh4");
 var timeEl = document.getElementById("timeRemaining");
 var favoriteEl = document.createElement("div");
 var listEl = document.createElement("ol");
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
+// var li1 = document.createElement("li");
+// var li2 = document.createElement("li");
+// var li3 = document.createElement("li");
+// var li4 = document.createElement("li");
+var answerOptions = document.getElementById("answer-options");
+var currentQuestionIndex = 0;
+
+// var score = 0;
+var timerInterval;
+var secondsLeft = 60;
+
+var questions = [
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        options: ["backslashes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+
+    {
+        question: "Commonly used data types do NOT include:",
+        options: ["strings", "booleans", "alert", "numbers"],
+        answer: "alert"
+    },
+
+    {
+        question: "Arrays in javascript can be used to store ____.",
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        options: ["commas", "curly brackets", "quotes", "asterisks"],
+        answer: "quotes"
+    },
+
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+];
+
+function handleAnswer() {
+    if (this.value === questions[currentQuestionIndex].answer) {
+        console.log("correct!")
+    } else {
+        console.log("incorrect!")
+        secondsLeft -= 10;
+        timeEl.textContent = secondsLeft;
+    }
+
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex === questions.length) {
+        gameOver();
+    } else {
+        displayQuestions();
+    }
+};
+
+function displayQuestions() {
+    var currentQuestion = questions[currentQuestionIndex];
+    listEl.textContent = currentQuestion.question;
+    currentQuestion.options.forEach(element => {
+        var li = document.createElement('button');
+        li.textContent = element;
+        li.setAttribute("value", element);
+        li.onclick = handleAnswer;
+        listEl.appendChild(li);
+    });
+}
+
+function gameOver() {
+    clearInterval(timerInterval);
+    myH1.textContent = "GAME OVER!"
+    timeEl.textContent = "";
+    favoriteEl.removeChild(listEl);
+    mySection.removeChild(timeEl);
+    // set score to secondsLeft
+    // display score
+    // add input to take in user initials
+    // submit button that fires a save score function to grab value of initials and score and set it into local storage
+        // create function to get initials and scores from local storage and re-render to page as a high score section
+    // add button to restart game
 
 
-// function gameOver() {
-    // code block
-// }
-
-
-
+}
 
 // starts the first question
 startButton.addEventListener("click", function() {
 
+    // remove some of the starting html elements
     mySection.removeChild(myP);
     myButton.removeChild(startButton);
     myFooter.removeChild(footerH4);
 
-    var timerInterval = setInterval(function() {
+    displayQuestions();
+
+    // adds new html elements
+    body.appendChild(favoriteEl);
+    favoriteEl.appendChild(listEl);
+    listEl.setAttribute("class", "answer-options");
+    // listEl.appendChild(li1);
+    // listEl.appendChild(li2);
+    // listEl.appendChild(li3);
+    // listEl.appendChild(li4);
+    
+    // li1.addEventListener("click", handleAnswer)
+    // li2.addEventListener("click", handleAnswer)
+    // li3.addEventListener("click", handleAnswer)
+    // li4.addEventListener("click", handleAnswer)
+
+    // favoriteEl.setAttribute("style", "font-size:20px;");
+    // listEl.setAttribute("style", "padding:20px;");
+    // li1.setAttribute("style", "padding: 5px; margin-left: 35px;");
+    // li2.setAttribute("style", "padding: 5px; margin-left: 35px;");
+    // li3.setAttribute("style", "padding: 5px; margin-left: 35px;");
+    // li4.setAttribute("style", "padding: 5px; margin-left: 35px;");
+
+    // starts the countdown timer
+    timerInterval = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = secondsLeft + " seconds remain";
     
@@ -39,193 +138,6 @@ startButton.addEventListener("click", function() {
             clearInterval(timerInterval);
             timeEl.textContent = "Your time is up.";
             // gameOver();
-        }
+        };
     }, 1000);
-
-    function question1() {
-        listEl.textContent = "The condition in an if / else statement is enclosed within ____.";
-        li1.textContent = "quotes";
-        li2.textContent = "curly brackets";
-        li3.textContent = "parentheses";
-        li4.textContent = "square brackets";
-    
-        // mySection.removeChild(myP);
-        // myButton.removeChild(startButton);
-        // myFooter.removeChild(footerH4);
-    
-        body.appendChild(favoriteEl);
-        favoriteEl.appendChild(listEl);
-        listEl.appendChild(li1);
-        listEl.appendChild(li2);
-        listEl.appendChild(li3);
-        listEl.appendChild(li4);
-    
-        favoriteEl.setAttribute("style", "font-size:20px;");
-        listEl.setAttribute("style", "padding:20px;");
-        li1.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li2.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li3.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li4.setAttribute("style", "padding: 5px; margin-left: 35px;");
-    }
-
-    question1();
-
-    function question2() {
-        listEl.textContent = "Commonly used data types do NOT include:";
-        li1.textContent = "strings";
-        li2.textContent = "booleans";
-        li3.textContent = "alerts";
-        li4.textContent = "numbers";
-    
-        // mySection.removeChild(myP);
-        // myButton.removeChild(startButton);
-        // myFooter.removeChild(footerH4);
-    
-        body.appendChild(favoriteEl);
-        favoriteEl.appendChild(listEl);
-        listEl.appendChild(li1);
-        listEl.appendChild(li2);
-        listEl.appendChild(li3);
-        listEl.appendChild(li4);
-    
-        favoriteEl.setAttribute("style", "font-size:20px;");
-        listEl.setAttribute("style", "padding:20px;");
-        li1.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li2.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li3.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li4.setAttribute("style", "padding: 5px; margin-left: 35px;");
-    }
-
-    function question3() {
-        listEl.textContent = "Arrays in javascript can be used to store ____.";
-        li1.textContent = "numbers and strings";
-        li2.textContent = "other arrays";
-        li3.textContent = "booleans";
-        li4.textContent = "all of the above";
-    
-        // mySection.removeChild(myP);
-        // myButton.removeChild(startButton);
-        // myFooter.removeChild(footerH4);
-    
-        body.appendChild(favoriteEl);
-        favoriteEl.appendChild(listEl);
-        listEl.appendChild(li1);
-        listEl.appendChild(li2);
-        listEl.appendChild(li3);
-        listEl.appendChild(li4);
-    
-        favoriteEl.setAttribute("style", "font-size:20px;");
-        listEl.setAttribute("style", "padding:20px;");
-        li1.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li2.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li3.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li4.setAttribute("style", "padding: 5px; margin-left: 35px;");
-    }
-
-    function question4() {
-        listEl.textContent = "String values must be enclosed within ____ when being assigned to variables.";
-        li1.textContent = "commas";
-        li2.textContent = "curly brackets";
-        li3.textContent = "quotes";
-        li4.textContent = "parentheses";
-    
-        // mySection.removeChild(myP);
-        // myButton.removeChild(startButton);
-        // myFooter.removeChild(footerH4);
-    
-        body.appendChild(favoriteEl);
-        favoriteEl.appendChild(listEl);
-        listEl.appendChild(li1);
-        listEl.appendChild(li2);
-        listEl.appendChild(li3);
-        listEl.appendChild(li4);
-    
-        favoriteEl.setAttribute("style", "font-size:20px;");
-        listEl.setAttribute("style", "padding:20px;");
-        li1.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li2.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li3.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li4.setAttribute("style", "padding: 5px; margin-left: 35px;");
-    }
-
-    function question5() {
-        listEl.textContent = "A very useful tool used during development and debugging for printing content to the debugger is:";
-        li1.textContent = "JavaScript";
-        li2.textContent = "terminal/bash";
-        li3.textContent = "for loops";
-        li4.textContent = "console.log";
-    
-        // mySection.removeChild(myP);
-        // myButton.removeChild(startButton);
-        // myFooter.removeChild(footerH4);
-    
-        body.appendChild(favoriteEl);
-        favoriteEl.appendChild(listEl);
-        listEl.appendChild(li1);
-        listEl.appendChild(li2);
-        listEl.appendChild(li3);
-        listEl.appendChild(li4);
-    
-        favoriteEl.setAttribute("style", "font-size:20px;");
-        listEl.setAttribute("style", "padding:20px;");
-        li1.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li2.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li3.setAttribute("style", "padding: 5px; margin-left: 35px;");
-        li4.setAttribute("style", "padding: 5px; margin-left: 35px;");
-    }
-    
-    // var questions = [question1, question2, question3, question4, question5];
-
-    // for (var i = 0; i < questions.length; i++) {
-        // questions[i]
-    // }
-
 });
-
-
-
-
-// myH1.style.color = 'pink';
-// myH1.setAttribute("id", "my-heading");
-// myH1.setAttribute("style", "color: blue; font-size: 30px");
-// console.log(a.getAttribute("href"));
-// window.addEventListener("mouseover", function() {
-    // code block
-// })
-
-
-
-/*
-the condition in an if / else statement is enclosed within ____.
-1. quotes
-2. curly brackets
-3. parentheses
-4. square brackets
-
-commonly used data types do NOT include:
-1. strings
-2. booleans
-3. alerts
-4. numbers
-
-arrays in javascript can be used to store ____.
-1. numbers and strings
-2. other arrays
-3. booleans
-4. all of the above
-
-string values must be enclosed within ____ when being assigned to variables
-1. commas
-2. curly brackets
-3. quotes
-4. parentheses
-
-a very useful tool used during development and debugging for printing content to the debugger is:
-1. JavaScript
-2. terminal/bash
-3. for loops
-4. console.log
-
-All done! Final Score, etc
-*/
-
